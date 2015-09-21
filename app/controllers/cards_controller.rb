88ffbd67eaca8b3b1bc19205b432cd4dcf8c5ade
isnoto <noto.rsv@gmail.com>
2015-09-21 22:09:50 +0300

    Change erb to slim and add CRUD for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,53 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def show
+    @card = find_card
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+    @card = find_card
+    render :show
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to cards_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @card = find_card
+
+    if @card.update(card_params)
+      redirect_to card_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card = find_card
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+
+  def find_card
+    Card.find(params[:id])
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,15 @@
 class Card < ActiveRecord::Base
+  after_validation :validate_text, :add_review_date, if: :new_record?
+  validates :original_text, :translated_text, presence: true
+
+
+  private
+
+  def validate_text
+    original_text.downcase != translated_text.downcase
+  end
+
+  def add_review_date
+    self.review_date = Time.now + 3.days
+  end
 end
