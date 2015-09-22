f37e0c79e23261a25eccf5ff2aac952533c16587
isnoto <noto.rsv@gmail.com>
2015-09-22 21:46:17 +0300

    Some fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 source = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 doc = Nokogiri::HTML(open(source))
 
-
 words = doc.css("tbody tr")[1..-1]
 words.each do |word|
-  Card.create(original_text: word.children[3].text, translated_text: word.children[5].text)
+  Card.create(original_text: word.children[3].text,
+              translated_text: word.children[5].text)
 end
