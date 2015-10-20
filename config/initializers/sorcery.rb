d82dff880bcaa536db4d0f49cacd515dd5de24b8
isnoto <noto.rsv@gmail.com>
2015-10-20 23:18:25 +0200

    Some fixes

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -8,12 +8,12 @@ Rails.application.config.sorcery.configure do |config|
   config.twitter.key = Rails.application.secrets.twitter_app_key
   config.twitter.secret = Rails.application.secrets.twitter_app_secret
   config.twitter.callback_url = Rails.application.secrets.twitter_app_callback_url
-  config.twitter.user_info_mapping = {name: 'screen_name'}
+  config.twitter.user_info_mapping = { name: 'screen_name' }
 
   config.facebook.key = Rails.application.secrets.facebook_app_key
   config.facebook.secret = Rails.application.secrets.facebook_app_secret
   config.facebook.callback_url = Rails.application.secrets.facebook_app_callback_url
-  config.facebook.user_info_mapping = {email: 'email', name: 'first_name'}
+  config.facebook.user_info_mapping = { email: 'email', name: 'first_name' }
   config.facebook.scope = [:email]
   config.facebook.display = 'page'
   config.facebook.user_info_path = 'me?fields=id,email,first_name'
