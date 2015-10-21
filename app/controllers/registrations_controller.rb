972477510ae24db1536e1f8e001406d874bef531
isnoto <noto.rsv@gmail.com>
2015-10-21 17:07:36 +0200

    Add registrations and profile controllers instead of users controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
@@ -11,18 +11,6 @@ class UsersController < ApplicationController
       redirect_to root_path, notice: 'Добро пожаловать!'
     else
       render 'new'
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if current_user.update(user_params)
-      redirect_to edit_user_path, notice: 'Данные обновленны'
-    else
-      flash.now[:alert] = 'Данные заполнены некорректно!'
-      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,69 +3,15 @@ Rails.application.routes.draw do
 
   resources :reviews,       only: [:show, :create]
   resources :cards
-  resources :users,         only: [:new, :create, :edit, :update]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :registrations, only: [:new, :create]
+  resources :profile,       only: [:edit, :update]
+  resources :sessions,      only: [:new, :create, :destroy]
 
-  get '/sign_up', to: 'users#new', as: 'sign_up'
+  get '/sign_up', to: 'registrations#new', as: 'sign_up'
   get '/log_in', to: 'sessions#new', as: :log_in
   delete '/log_out', to: 'sessions#destroy', as: :log_out
 
   post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
-  get 'oauth/:provider', to: 'oauths#oauth', :as => :auth_at_provider
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
 end
