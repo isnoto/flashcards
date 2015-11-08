65c67671abf1b073400b635eba0bbee5177cc95c
isnoto <noto.rsv@gmail.com>
2015-11-08 23:49:09 +0200

    Fix environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_key,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: Rails.application.secrets.aws_region
     }
     config.fog_directory = Rails.application.secrets.fog_directory
