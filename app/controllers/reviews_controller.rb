0a08c59146ce2f440025b3de02764474f5bda58f
isnoto <noto.rsv@gmail.com>
2015-11-14 19:19:16 +0200

    Style fixes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,17 +9,17 @@ class ReviewsController < ApplicationController
     @card = Card.find(review_params[:card_id])
 
     case @card.check_answer(review_params[:answer])
-      when :correct_answer
-        redirect_to root_path, notice: 'Верно!'
-      when :typo_in_word
-        flash.now[:remind] = "Возможно вы ввиду слово #{ @card.original_text }."
-        render :show
-      when :wrong_answer
-        redirect_to root_path, alert: 'Ваш ответ не правильный'
-      when :wrong_answers_streak
-        flash[:alert] = 'Вы ввели три раза не правильно!
-                            Сделующая дата пересмотра карточки через 12 часов.'
-        redirect_to root_path
+    when :correct_answer
+      redirect_to root_path, notice: 'Верно!'
+    when :typo_in_word
+      flash.now[:remind] = "Возможно вы ввиду слово #{@card.original_text}."
+      render :show
+    when :wrong_answer
+      redirect_to root_path, alert: 'Ваш ответ не правильный'
+    when :wrong_answers_streak
+      flash[:alert] = 'Вы ввели три раза не правильно!
+                          Сделующая дата пересмотра карточки через 12 часов.'
+      redirect_to root_path
     end
   end
 
