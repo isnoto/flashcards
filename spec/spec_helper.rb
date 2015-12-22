47da9db1350da0986e97f20e97f763dcca3c5b5b
isnoto <noto.rsv@gmail.com>
2015-12-22 20:36:36 +0200

    Restore old settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {debug: true, default_wait_time: 30, timeout: 90})
-end
 
 RSpec.configure do |config|
   Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
